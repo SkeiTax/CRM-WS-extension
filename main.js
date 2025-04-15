a3bc7753ca33c9031125c72b200de16ad3566008
Андрей Дорофеев <adorofeev.dev@dent-it.ru>
2025-04-15 18:32:55 +0300

    Added init function in main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log("Hello user!")
+// crm-tooltip
+
+
+function waitForElm(selector) {
+	return new Promise(resolve => {
+		if (document.querySelector(selector)) {
+			return resolve(document.querySelector(selector));
+		}
+		const observer = new MutationObserver(mutations => {
+			if (document.querySelector(selector)) {
+				resolve(document.querySelector(selector));
+				observer.disconnect();
+			}
+		});
+		observer.observe(document.body, {
+			childList: true,
+			subtree: true
+		});
+	});
+}
+
+async function init (){
+	var element = await waitForElm('#MainDiv table')
+	console.log(element)
+}
+
+init()
