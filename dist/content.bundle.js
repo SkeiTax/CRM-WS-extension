0df5a0002007d7bc4ab65685f5043fc294255388
Андрей Дорофеев <dorifeef1@gmail.com>
2025-05-21 20:17:36 +0300

    Добавлена возможность получить дамп данных расширения через: window.postMessage({ type: "CRMEDump_CALL" }, "*");

--- dist/content.bundle.js
+++ dist/content.bundle.js
@@ -23221,131 +23221,158 @@
           month: Number(document.querySelector('#Month').value)
       });
   }
-  async function init() {
-      var table = (await waitForElm("#MainDiv > table"));
-      var filterDate = GetFilterDate();
-      var monthInfo = new MonthInfo(table, filterDate);
-      // try {
-      // 	console.log(monthInfo.days)
-      // 	console.log(monthInfo.days[4])
-      // 	console.log(monthInfo.days[4].mergedRanges)
-      // 	console.log(monthInfo.days[4].duration.toFormat("hh:mm"))
-      // 	console.log(monthInfo.TotalWorkDuration.toFormat("hh:mm"))
-      // }catch(e){console.error(e)}
-      var workDaysTracker = await waitForElm("#MainDiv > .crm-tooltip");
-      // var totalWorkDayToolTip = (await waitForElm(
-      // 	"#MainDiv > .crm-tooltip > div",
-      // )) as HTMLDivElement;
-      var worksDay = monthInfo.DaysWorked.length;
-      var totalDelta = monthInfo.TotalWorkDuration.minus(Duration.fromMillis(worksDay * 8 * 3600000));
-      var totalDeltaTimePrefix = totalDelta.toMillis() < 0 ? "Недоработка" : "Переработка";
-      console.log(totalDeltaTimePrefix, abs(totalDelta).toFormat("hh:mm"));
-      console.log(DateTime.now().zone);
-      var totalDeltaElement = document.createElement("span");
-      totalDeltaElement.textContent = `(${totalDeltaTimePrefix}: ${abs(totalDelta).toFormat("hh:mm")})`;
-      totalDeltaElement.setAttribute("style", "color: #aaa; font-size: 0.8em;");
-      workDaysTracker.appendChild(totalDeltaElement);
-      //SetTimeInTotalToolTip(totalWorkDayToolTip, offline, online);
-      var chartDiv = document.createElement("div");
-      chartDiv.setAttribute("style", "width: 800px; height: 400px;");
-      var canvas = document.createElement("canvas");
-      chartDiv.appendChild(canvas);
-      (await waitForElm("#MainDiv")).append(chartDiv);
-      function daysStartWork() {
-          var data = monthInfo.days.map(day => {
-              var ranges = day.mergedRanges.filter(range => range.begin != undefined);
-              return {
-                  y: ranges.length == 0
-                      ? undefined
-                      : ranges[0].begin?.minus(day.date.toMillis()),
-                  x: day.date?.toISO()
-              };
-          });
-          return data;
+  class CRME {
+      static _instance;
+      monthInfo;
+      constructor(monthInfo) {
+          this.monthInfo = monthInfo;
       }
-      function daysEndWork() {
-          var data = monthInfo.days.map(day => {
-              var ranges = day.mergedRanges.filter(range => range.end != undefined);
-              return {
-                  y: ranges.length == 0
-                      ? undefined
-                      : ranges[ranges.length - 1].end?.minus(day.date.toMillis()),
-                  x: day.date?.toISO()
-              };
-          });
-          return data;
-      }
-      var ctx = canvas.getContext('2d');
-      const gradient = ctx.createLinearGradient(0, 0, 0, 400); // вертикальный градиент
-      gradient.addColorStop(0, 'rgba(210, 180, 140, 0.75)'); // сверху — полупрозрачный зеленый
-      gradient.addColorStop(1, 'rgba(116, 158, 98, 0)'); // снизу — прозрачный
-      new Chart(canvas, {
-          type: 'line',
-          data: {
-              datasets: [{
-                      label: 'Начало рабочего дня',
-                      data: daysStartWork(),
-                      fill: "+1",
-                      backgroundColor: gradient,
-                      borderColor: 'rgb(116, 158, 98)',
-                      tension: 0.2
-                  },
-                  {
-                      label: 'Конец рабочего дня',
-                      data: daysEndWork(),
-                      fill: false,
-                      borderColor: 'rgb(210, 180, 140)',
-                      tension: 0.2
-                  }]
-          },
-          options: {
-              scales: {
-                  x: {
-                      adapters: {
-                          date: {
-                              zone: "UTC",
-                              setZone: true
-                          },
+      static async Instance() {
+          if (CRME._instance !== undefined)
+              return CRME._instance;
+          var table = (await waitForElm("#MainDiv > table"));
+          var filterDate = GetFilterDate();
+          var monthInfo = new MonthInfo(table, filterDate);
+          CRME._instance = new CRME(monthInfo);
+          // try {
+          // 	console.log(monthInfo.days)
+          // 	console.log(monthInfo.days[4])
+          // 	console.log(monthInfo.days[4].mergedRanges)
+          // 	console.log(monthInfo.days[4].duration.toFormat("hh:mm"))
+          // 	console.log(monthInfo.TotalWorkDuration.toFormat("hh:mm"))
+          // }catch(e){console.error(e)}
+          var workDaysTracker = await waitForElm("#MainDiv > .crm-tooltip");
+          // var totalWorkDayToolTip = (await waitForElm(
+          // 	"#MainDiv > .crm-tooltip > div",
+          // )) as HTMLDivElement;
+          var worksDay = monthInfo.DaysWorked.length;
+          var totalDelta = monthInfo.TotalWorkDuration.minus(Duration.fromMillis(worksDay * 8 * 3600000));
+          var totalDeltaTimePrefix = totalDelta.toMillis() < 0 ? "Недоработка" : "Переработка";
+          console.log(totalDeltaTimePrefix, abs(totalDelta).toFormat("hh:mm"));
+          console.log(DateTime.now().zone);
+          var totalDeltaElement = document.createElement("span");
+          totalDeltaElement.textContent = `(${totalDeltaTimePrefix}: ${abs(totalDelta).toFormat("hh:mm")})`;
+          totalDeltaElement.setAttribute("style", "color: #aaa; font-size: 0.8em;");
+          workDaysTracker.appendChild(totalDeltaElement);
+          //SetTimeInTotalToolTip(totalWorkDayToolTip, offline, online);
+          var chartDiv = document.createElement("div");
+          chartDiv.setAttribute("style", "width: 800px; height: 400px;");
+          var canvas = document.createElement("canvas");
+          chartDiv.appendChild(canvas);
+          (await waitForElm("#MainDiv")).append(chartDiv);
+          function daysStartWork() {
+              var data = monthInfo.days.map(day => {
+                  var ranges = day.mergedRanges.filter(range => range.begin != undefined);
+                  return {
+                      y: ranges.length == 0
+                          ? undefined
+                          : ranges[0].begin?.minus(day.date.toMillis()),
+                      x: day.date?.toISO()
+                  };
+              });
+              return data;
+          }
+          function daysEndWork() {
+              var data = monthInfo.days.map(day => {
+                  var ranges = day.mergedRanges.filter(range => range.end != undefined);
+                  return {
+                      y: ranges.length == 0
+                          ? undefined
+                          : ranges[ranges.length - 1].end?.minus(day.date.toMillis()),
+                      x: day.date?.toISO()
+                  };
+              });
+              return data;
+          }
+          var ctx = canvas.getContext('2d');
+          const gradient = ctx.createLinearGradient(0, 0, 0, 400); // вертикальный градиент
+          gradient.addColorStop(0, 'rgba(210, 180, 140, 0.75)'); // сверху — полупрозрачный зеленый
+          gradient.addColorStop(1, 'rgba(116, 158, 98, 0)'); // снизу — прозрачный
+          new Chart(canvas, {
+              type: 'line',
+              data: {
+                  datasets: [{
+                          label: 'Начало рабочего дня',
+                          data: daysStartWork(),
+                          fill: "+1",
+                          backgroundColor: gradient,
+                          borderColor: 'rgb(116, 158, 98)',
+                          tension: 0.2
                       },
-                      type: 'time',
-                      time: {
-                          unit: 'day',
-                          displayFormats: {
-                              day: 'dd.MM'
+                      {
+                          label: 'Конец рабочего дня',
+                          data: daysEndWork(),
+                          fill: false,
+                          borderColor: 'rgb(210, 180, 140)',
+                          tension: 0.2
+                      }]
+              },
+              options: {
+                  scales: {
+                      x: {
+                          adapters: {
+                              date: {
+                                  zone: "UTC",
+                                  setZone: true
+                              },
                           },
-                      },
-                      title: {
-                          display: true,
-                          text: 'Дата'
-                      }
-                  },
-                  y: {
-                      adapters: {
-                          date: {
-                              zone: "UTC",
-                              setZone: true
+                          type: 'time',
+                          time: {
+                              unit: 'day',
+                              displayFormats: {
+                                  day: 'dd.MM'
+                              },
                           },
+                          title: {
+                              display: true,
+                              text: 'Дата'
+                          }
                       },
-                      type: 'time',
-                      time: {
-                          unit: 'hour',
-                          displayFormats: {
-                              hour: 'HH:mm'
+                      y: {
+                          adapters: {
+                              date: {
+                                  zone: "UTC",
+                                  setZone: true
+                              },
                           },
-                          tooltipFormat: 'HH:mm',
-                          // parser: function (value) {
-                          // 	if (value === 0) return 0
-                          // 	var time = value as DateTime
-                          // 	if (time === undefined) return 0
-                          // 	return time.toUTC().toMillis();
-                          // }
-                      },
-                      title: {
-                          display: true,
-                          text: 'Время'
+                          type: 'time',
+                          time: {
+                              unit: 'hour',
+                              displayFormats: {
+                                  hour: 'HH:mm'
+                              },
+                              tooltipFormat: 'HH:mm',
+                              // parser: function (value) {
+                              // 	if (value === 0) return 0
+                              // 	var time = value as DateTime
+                              // 	if (time === undefined) return 0
+                              // 	return time.toUTC().toMillis();
+                              // }
+                          },
+                          title: {
+                              display: true,
+                              text: 'Время'
+                          }
                       }
                   }
               }
+          });
+          return CRME._instance;
+      }
+      dump = () => {
+          return JSON.stringify(this.monthInfo);
+      };
+  }
+  async function init() {
+      const crme = await CRME.Instance();
+      window.addEventListener("message", (event) => {
+          if (event.source !== window)
+              return;
+          if (event.data.type === "CRMEDump_CALL") {
+              console.log("Method called via postMessage");
+              console.log(crme.dump());
+              // можно ответить
+              window.postMessage({ type: "CRMEDump_RESPONSE", data: "ok" }, "*");
           }
       });
   }
--- src/index.ts
+++ src/index.ts
@@ -29,154 +29,185 @@ function GetFilterDate(){
 	})
 }
 
-async function init() {
-	var table = (await waitForElm("#MainDiv > table")) as HTMLTableElement;
-	var filterDate = GetFilterDate()
+class CRME {
+	private static _instance: CRME
+	public monthInfo: MonthInfo;
 
-	var monthInfo = new MonthInfo(table, filterDate)
-
-	// try {
-	// 	console.log(monthInfo.days)
-	// 	console.log(monthInfo.days[4])
-	// 	console.log(monthInfo.days[4].mergedRanges)
-	// 	console.log(monthInfo.days[4].duration.toFormat("hh:mm"))
-	// 	console.log(monthInfo.TotalWorkDuration.toFormat("hh:mm"))
-	// }catch(e){console.error(e)}
-
-	var workDaysTracker = await waitForElm("#MainDiv > .crm-tooltip");
-	// var totalWorkDayToolTip = (await waitForElm(
-	// 	"#MainDiv > .crm-tooltip > div",
-	// )) as HTMLDivElement;
-
-	var worksDay = monthInfo.DaysWorked.length
-	
-	var totalDelta = monthInfo.TotalWorkDuration.minus(Duration.fromMillis(worksDay * 8 * 3600000))
-
-	var totalDeltaTimePrefix = totalDelta.toMillis() < 0 ? "Недоработка" : "Переработка";
-
-	console.log(totalDeltaTimePrefix, abs(totalDelta).toFormat("hh:mm"));
-	console.log(DateTime.now().zone)
-
-	var totalDeltaElement = document.createElement("span");
-	totalDeltaElement.textContent = `(${totalDeltaTimePrefix}: ${abs(totalDelta).toFormat("hh:mm")})`;
-	totalDeltaElement.setAttribute("style", "color: #aaa; font-size: 0.8em;");
-
-	workDaysTracker.appendChild(totalDeltaElement);
-	//SetTimeInTotalToolTip(totalWorkDayToolTip, offline, online);
-
-	var chartDiv = document.createElement("div") as HTMLDivElement;
-	chartDiv.setAttribute("style", "width: 800px; height: 400px;");
-	var canvas = document.createElement("canvas") as HTMLCanvasElement;
-	chartDiv.appendChild(canvas);
-
-	(await waitForElm("#MainDiv")).append(chartDiv)
-
-	function chartLabels(){
-		var data = monthInfo.days.map(day => `${day.number}.${filterDate.month}`) 
-		return data
+	constructor(monthInfo: MonthInfo){
+		this.monthInfo = monthInfo
 	}
 
-	function daysStartWork(){
-		var data = monthInfo.days.map(day=>{
-			var ranges = day.mergedRanges.filter(range=>range.begin != undefined)
-			return {
-				y: ranges.length == 0 
-					? undefined
-					: ranges[0].begin?.minus(day.date.toMillis()),
-				x: day.date?.toISO()
-			}
-		})
-		return data
-	}
+	public static async Instance() {
+		if (CRME._instance !== undefined) return CRME._instance
+		var table = (await waitForElm("#MainDiv > table")) as HTMLTableElement;
+		var filterDate = GetFilterDate()
 
-	function daysEndWork(){
-		var data = monthInfo.days.map(day=>{
-			var ranges = day.mergedRanges.filter(range=>range.end != undefined)
-			return {
-				y: ranges.length == 0 
-					? undefined
-					: ranges[ranges.length-1].end?.minus(day.date.toMillis()),
-				x: day.date?.toISO()
-			}
-		})
-		return data
-	}
+		var monthInfo = new MonthInfo(table, filterDate)
+		CRME._instance = new CRME(monthInfo)
 
-	var ctx = canvas.getContext('2d')!
+		// try {
+		// 	console.log(monthInfo.days)
+		// 	console.log(monthInfo.days[4])
+		// 	console.log(monthInfo.days[4].mergedRanges)
+		// 	console.log(monthInfo.days[4].duration.toFormat("hh:mm"))
+		// 	console.log(monthInfo.TotalWorkDuration.toFormat("hh:mm"))
+		// }catch(e){console.error(e)}
 
-	const gradient = ctx.createLinearGradient(0, 0, 0, 400);  // вертикальный градиент
-	gradient.addColorStop(0, 'rgba(210, 180, 140, 0.75)');      // сверху — полупрозрачный зеленый
-	gradient.addColorStop(1, 'rgba(116, 158, 98, 0)');        // снизу — прозрачный
+		var workDaysTracker = await waitForElm("#MainDiv > .crm-tooltip");
+		// var totalWorkDayToolTip = (await waitForElm(
+		// 	"#MainDiv > .crm-tooltip > div",
+		// )) as HTMLDivElement;
 
-	new Chart(canvas, {
-		type: 'line',
-		data:{
-			datasets: [{
-				label: 'Начало рабочего дня',
-				data: daysStartWork(),
-				fill: "+1",
-				backgroundColor: gradient,
-				borderColor: 'rgb(116, 158, 98)',
-				tension: 0.2
-			},
-			{
-				label: 'Конец рабочего дня',
-				data: daysEndWork(),
-				fill: false,
-				borderColor: 'rgb(210, 180, 140)',
-				tension: 0.2
-			}]
-		},
-		options:{
-			scales: {
-				x: {
-					adapters: {
-						date: {
-							zone: "UTC",
-							setZone: true
-						},
-					},
-					type: 'time',
-					time: {
-						unit: 'day',
-						displayFormats: {
-							day: 'dd.MM'
-						},
-					},
-					title: {
-						display: true,
-						text: 'Дата'
-					}
+		var worksDay = monthInfo.DaysWorked.length
+
+		var totalDelta = monthInfo.TotalWorkDuration.minus(Duration.fromMillis(worksDay * 8 * 3600000))
+
+		var totalDeltaTimePrefix = totalDelta.toMillis() < 0 ? "Недоработка" : "Переработка";
+
+		console.log(totalDeltaTimePrefix, abs(totalDelta).toFormat("hh:mm"));
+		console.log(DateTime.now().zone)
+
+		var totalDeltaElement = document.createElement("span");
+		totalDeltaElement.textContent = `(${totalDeltaTimePrefix}: ${abs(totalDelta).toFormat("hh:mm")})`;
+		totalDeltaElement.setAttribute("style", "color: #aaa; font-size: 0.8em;");
+
+		workDaysTracker.appendChild(totalDeltaElement);
+		//SetTimeInTotalToolTip(totalWorkDayToolTip, offline, online);
+
+		var chartDiv = document.createElement("div") as HTMLDivElement;
+		chartDiv.setAttribute("style", "width: 800px; height: 400px;");
+		var canvas = document.createElement("canvas") as HTMLCanvasElement;
+		chartDiv.appendChild(canvas);
+
+		(await waitForElm("#MainDiv")).append(chartDiv)
+
+		function chartLabels() {
+			var data = monthInfo.days.map(day => `${day.number}.${filterDate.month}`)
+			return data
+		}
+
+		function daysStartWork() {
+			var data = monthInfo.days.map(day => {
+				var ranges = day.mergedRanges.filter(range => range.begin != undefined)
+				return {
+					y: ranges.length == 0
+						? undefined
+						: ranges[0].begin?.minus(day.date.toMillis()),
+					x: day.date?.toISO()
+				}
+			})
+			return data
+		}
+
+		function daysEndWork() {
+			var data = monthInfo.days.map(day => {
+				var ranges = day.mergedRanges.filter(range => range.end != undefined)
+				return {
+					y: ranges.length == 0
+						? undefined
+						: ranges[ranges.length - 1].end?.minus(day.date.toMillis()),
+					x: day.date?.toISO()
+				}
+			})
+			return data
+		}
+
+		var ctx = canvas.getContext('2d')!
+
+		const gradient = ctx.createLinearGradient(0, 0, 0, 400);  // вертикальный градиент
+		gradient.addColorStop(0, 'rgba(210, 180, 140, 0.75)');      // сверху — полупрозрачный зеленый
+		gradient.addColorStop(1, 'rgba(116, 158, 98, 0)');        // снизу — прозрачный
+
+		new Chart(canvas, {
+			type: 'line',
+			data: {
+				datasets: [{
+					label: 'Начало рабочего дня',
+					data: daysStartWork(),
+					fill: "+1",
+					backgroundColor: gradient,
+					borderColor: 'rgb(116, 158, 98)',
+					tension: 0.2
 				},
-				y: {
-					adapters: {
-						date: {
-							zone: "UTC",
-							setZone: true
+				{
+					label: 'Конец рабочего дня',
+					data: daysEndWork(),
+					fill: false,
+					borderColor: 'rgb(210, 180, 140)',
+					tension: 0.2
+				}]
+			},
+			options: {
+				scales: {
+					x: {
+						adapters: {
+							date: {
+								zone: "UTC",
+								setZone: true
+							},
 						},
-					},
-					type: 'time',
-					time: {
-						unit: 'hour',
-						displayFormats: {
-							hour: 'HH:mm'
+						type: 'time',
+						time: {
+							unit: 'day',
+							displayFormats: {
+								day: 'dd.MM'
+							},
 						},
-						tooltipFormat: 'HH:mm',
-						// parser: function (value) {
-						// 	if (value === 0) return 0
-						// 	var time = value as DateTime
-						// 	if (time === undefined) return 0
-						// 	return time.toUTC().toMillis();
-						// }
+						title: {
+							display: true,
+							text: 'Дата'
+						}
 					},
-					title: {
-						display: true,
-						text: 'Время'
+					y: {
+						adapters: {
+							date: {
+								zone: "UTC",
+								setZone: true
+							},
+						},
+						type: 'time',
+						time: {
+							unit: 'hour',
+							displayFormats: {
+								hour: 'HH:mm'
+							},
+							tooltipFormat: 'HH:mm',
+							// parser: function (value) {
+							// 	if (value === 0) return 0
+							// 	var time = value as DateTime
+							// 	if (time === undefined) return 0
+							// 	return time.toUTC().toMillis();
+							// }
+						},
+						title: {
+							display: true,
+							text: 'Время'
+						}
 					}
 				}
 			}
-		}
-	})
+		})
+		 return CRME._instance
+	}
+
+	public dump = ()=>{
+		return JSON.stringify(this.monthInfo);
+	}
 }
 
-init();
+async function init(){
+	const crme = await CRME.Instance()
+
+	window.addEventListener("message", (event) => {
+	if (event.source !== window) return;
+	if (event.data.type === "CRMEDump_CALL") {
+		console.log("Method called via postMessage");
+		console.log(crme.dump());
+		// можно ответить
+		window.postMessage({ type: "CRMEDump_RESPONSE", data: "ok" }, "*");
+	}
+	});
+}
+
+init()
+
