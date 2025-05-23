de9a121d5da8f7d19aa13772cafe04a49fff6e48
Андрей Дорофеев <dorifeef1@gmail.com>
2025-05-23 22:50:13 +0300

    Добавление style.css обновлены пакеты Обновлен процес сборки в dist dist добавлен в gitignore

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/index.ts',
@@ -13,6 +14,12 @@ export default {
   plugins: [
     resolve(),     // ищет зависимости в node_modules
     commonjs(),    // преобразует commonjs-модули в ES-модули
-    typescript()
+    typescript(),
+    copy({
+      targets: [
+        { src: 'manifest.json', dest: 'dist' },
+        { src: 'resources/*', dest: 'dist' }
+      ]
+    })
   ],
 };
