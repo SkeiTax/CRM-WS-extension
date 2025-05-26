d17a69d1732a09926b049f3d6187f0432a601f90
Андрей Дорофеев <dorifeef1@gmail.com>
2025-05-26 15:05:31 +0300

    Добавлен компонент графиков Chart

---
+++ src/Components/Chart.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="canvas-comp">
+    <canvas class="canvas-content" ref="canvasRef"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import Chart, { ChartType } from 'chart.js/auto'
+
+import type { ChartData, ChartOptions, Plugin } from 'chart.js';
+
+const props = defineProps<{
+  type: ChartType,
+  data: ChartData,
+  options?: ChartOptions,
+  plugins?: Plugin[]
+}>()
+const canvasRef = ref<HTMLCanvasElement | null>(null)
+
+onMounted(() => {
+  if (canvasRef.value) {
+    const ctx = canvasRef.value.getContext('2d')
+    if (ctx) {
+      new Chart(ctx, {
+        type: props.type,
+        data: props.data,
+        options: props.options,
+        plugins: props.plugins
+      })
+    }
+  }
+})
+</script>
+
+<style scoped>
+.canvas-comp{
+    width: 100%;
+}
+
+.canvas-content {
+  height: 100%;
+  width: 100%;
+  aspect-ratio: unset;
+}
+</style>
